5d7b1b9fc30d3ce41a1ee6381dd1bb43ae6ee6f8
Alex Plate <AlexPl292@gmail.com>
2021-05-05 15:35:55 +0300

    Update version 0.1.3

--- Formula/tg_backup.rb
+++ Formula/tg_backup.rb
@@ -1,9 +1,9 @@
 class TgBackup < Formula
   desc "Backup your messages from Telegram messenger"
   homepage "https://github.com/AlexPl292/tg_backup"
-  url "https://github.com/AlexPl292/tg_backup/releases/download/v0.1.2/tg_backup-x86_64-apple-darwin.zip"
-  sha256 "fb712c8b8c6d20faa448cb843b6a9fdff1328830d40b5e69f8f00e0615d3fff4"
-  version "0.1.2"
+  url "https://github.com/AlexPl292/tg_backup/releases/download/v0.1.3/tg_backup-x86_64-apple-darwin.zip"
+  sha256 "216d17526ae188cec6d2a7ae0a4678d438811e8c86391431184f5c4b7d088e59"
+  version "0.1.3"
 
   def install
     bin.install "tg_backup"
