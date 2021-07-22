f5930b3dc35931d0dba64589d7aab07b05f118b2
Alex Pláte <AlexPl292@gmail.com>
2021-07-22 23:15:31 +0300

    tg_backup 0.1.11
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tg_backup.rb
+++ Formula/tg_backup.rb
@@ -1,9 +1,9 @@
 class TgBackup < Formula
   desc "Backup your messages from Telegram messenger"
   homepage "https://github.com/AlexPl292/tg_backup"
-  url "https://github.com/AlexPl292/tg_backup/releases/download/v0.1.9/tg_backup-x86_64-apple-darwin.zip"
-  sha256 "6fff0a92683b1564b43a71abb1eddd54cfd30051f349607306e5cceaa077da98"
-  version "0.1.9"
+  url "https://github.com/AlexPl292/tg_backup/releases/download/v0.1.11/tg_backup-x86_64-apple-darwin.zip"
+  sha256 "ef089536a95977526cca2d85389cea86af9573848ea425666f5d45f3c53e6790"
+  version "0.1.11"
 
   def install
     bin.install "tg_backup"
